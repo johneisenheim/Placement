8e4d45d6e6c118649227652e5ee766cb5b23f24e
alma940 <alma_24_8@hotmail.com>
2014-11-12 17:30:49 +0100

    Implementazione dei get, set e toString.

--- APIServer/ServerWeb/src/java/it/unisa/tp/model/concrete/ConcretePermissions.java
+++ APIServer/ServerWeb/src/java/it/unisa/tp/model/concrete/ConcretePermissions.java
@@ -14,4 +14,74 @@ import it.unisa.tp.model.interfaces.Permissions;
  */
 public class ConcretePermissions implements Permissions{
     
+    private int idPermissions;
+    private String description;
+    private String classe;
+            
+    public ConcretePermissions (int idPermissions, String description, String classe){
+        this.idPermissions=idPermissions;
+        this.description=description;
+        this.classe=classe;
+    }
+           
+    public ConcretePermissions (){
+        
+    }
+
+    @Override
+    public int getPrimaryKey() {
+       return this.getIdPermissions();
+    }
+
+    @Override
+    public void setPrimaryKey(int primaryKey) {
+        this.setIdPermissions(primaryKey);
+    }
+
+    /**
+     * @return the idPermissions
+     */
+    public int getIdPermissions() {
+        return idPermissions;
+    }
+
+    /**
+     * @param idPermissions the idPermissions to set
+     */
+    public void setIdPermissions(int idPermissions) {
+        this.idPermissions = idPermissions;
+    }
+
+    /**
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * @return the classe
+     */
+    public String getClasse() {
+        return classe;
+    }
+
+    /**
+     * @param classe the classe to set
+     */
+    public void setClasse(String classe) {
+        this.classe = classe;
+    }
+    
+    public String toString(){
+        return ("IDPermissionss: \n"+idPermissions+"Description: \n"+description+"Class:\n"+classe);
+    }
+    
 }
