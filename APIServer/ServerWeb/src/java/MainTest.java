4cd302a63287a863149d083b1a5c45fa8e0e69e9
valucifer <v.vivone.5389@gmail.com>
2014-11-13 09:06:37 +0100

    ---

---
+++ APIServer/ServerWeb/src/java/MainTest.java
@@ -0,0 +1,32 @@
+
+import it.unisa.tp.control.AuthenticateUser;
+import it.unisa.tp.control.DBConnection;
+import it.unisa.tp.model.concrete.ConcreteAccount;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author carlosborges
+ */
+public class MainTest {
+    
+     public static void main(String[] args) {
+         try {
+             DBConnection.connect();
+         } catch (ClassNotFoundException ex) {
+             Logger.getLogger(MainTest.class.getName()).log(Level.SEVERE, null, ex);
+         } catch (SQLException ex) {
+             Logger.getLogger(MainTest.class.getName()).log(Level.SEVERE, null, ex);
+         } catch (IOException ex) {
+             Logger.getLogger(MainTest.class.getName()).log(Level.SEVERE, null, ex);
+         }
+    }
+}
