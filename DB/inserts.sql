b719bb032529ddd2ffa0064dcdc00b2ffa254a54
carboma89 <carboma89@gmail.com>
2014-11-17 13:18:44 +0100

    asda

--- DB/inserts.sql
+++ DB/inserts.sql
@@ -3,8 +3,9 @@ INSERT INTO `mydb`.`Account` (`idAccount`, `userName`, `password`, `typeOfAccoun
 INSERT INTO `mydb`.`StudentStatus` (`idStudentStatus`, `description`) VALUES ('1', 'Non richiesto');
 INSERT INTO `mydb`.`StudentStatus` (`idStudentStatus`, `description`) VALUES ('2', 'In attesa di conferma');
 INSERT INTO `mydb`.`StudentStatus` (`idStudentStatus`, `description`) VALUES ('3', 'Accettato');
-INSERT INTO `mydb`.`FisicPerson` (`idFisicPerson`, `name`, `lastName`) VALUES ('1', 'Valentino', 'Piglianculo');
 INSERT INTO `mydb`.`Department` (`idDepartment`) VALUES ('1');
+INSERT INTO `mydb`.`FisicPerson` (`idFisicPerson`, `name`, `lastName`) VALUES ('1', 'Valentino', 'Piglianculo');
+
 INSERT INTO `mydb`.`Student` (`serialNumber`, `FK_Account`, `FK_FisicPerson`, `FK_Department`, `FK_StudentStatus`) VALUES ('1', '1', '1', '1', '1');
 
 
