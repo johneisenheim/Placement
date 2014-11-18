826d47696dffe2ab707058041a679af002444710
carboma89 <carboma89@gmail.com>
2014-11-18 19:22:27 +0100

    jars per caricamento e creazione servlet
    Inserimento files jar per il caricamento tramite servlet creazione
    servlet (NON COMPLETATA)

--- APIServer/ServerWeb/src/java/it/unisa/control/servlet/UploadInformationFilesServlet.java
+++ APIServer/ServerWeb/src/java/it/unisa/control/servlet/UploadInformationFilesServlet.java
@@ -5,13 +5,22 @@
  */
 package it.unisa.control.servlet;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Iterator;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.io.output.*;
 
 /**
  *
@@ -19,6 +28,17 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet(name = "UploadInformationFilesServlet", urlPatterns = {"/UploadInformationFilesServlet"})
 public class UploadInformationFilesServlet extends HttpServlet {
+
+    private boolean isMultipart;
+    private String filePath;
+    private int maxFileSize = 50 * 1024;
+    private int maxMemSize = 4 * 1024;
+    private File file;
+
+    public void init() {
+        // Get the file location where it would be stored.
+        filePath = getServletContext().getInitParameter("file-upload");
+    }
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -38,7 +58,7 @@ public class UploadInformationFilesServlet extends HttpServlet {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet UploadInformationFilesServlet</title>");            
+            out.println("<title>Servlet UploadInformationFilesServlet</title>");
             out.println("</head>");
             out.println("<body>");
             out.println("<h1>Servlet UploadInformationFilesServlet at " + request.getContextPath() + "</h1>");
@@ -61,7 +81,9 @@ public class UploadInformationFilesServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        throw new ServletException("GET method used with " +
+                getClass( ).getName( )+": POST method required.");
+
     }
 
     /**
@@ -75,7 +97,55 @@ public class UploadInformationFilesServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        // Check that we have a file upload request
+      isMultipart = ServletFileUpload.isMultipartContent(request);
+      response.setContentType("text/html");
+      java.io.PrintWriter out = response.getWriter( );
+      DiskFileItemFactory factory = new DiskFileItemFactory();
+      // maximum size that will be stored in memory
+      factory.setSizeThreshold(maxMemSize);
+      // Location to save data that is larger than maxMemSize.
+      factory.setRepository(new File("c:\\temp"));
+      // Create a new file upload handler
+      ServletFileUpload upload = new ServletFileUpload(factory);
+      // maximum file size to be uploaded.
+      upload.setSizeMax( maxFileSize );
+      try{ 
+      // Parse the request to get file items.
+      List fileItems = upload.parseRequest(request);
+	
+      // Process the uploaded file items
+      Iterator i = fileItems.iterator();
+
+      while ( i.hasNext () ) 
+      {
+         FileItem fi = (FileItem)i.next();
+         if ( !fi.isFormField () )	
+         {
+            // Get the uploaded file parameters
+            String fieldName = fi.getFieldName();
+            String fileName = fi.getName();
+            String contentType = fi.getContentType();
+            boolean isInMemory = fi.isInMemory();
+            long sizeInBytes = fi.getSize();
+            // Write the file
+            if( fileName.lastIndexOf("\\") >= 0 ){
+               file = new File( filePath + 
+               fileName.substring( fileName.lastIndexOf("\\"))) ;
+            }else{
+               file = new File( filePath + 
+               fileName.substring(fileName.lastIndexOf("\\")+1)) ;
+            }
+            fi.write( file ) ;
+            out.println("Uploaded Filename: " + fileName + "<br>");
+         }
+      }
+      out.println("</body>");
+      out.println("</html>");
+   }catch(Exception ex) {
+       System.out.println(ex);
+   }
+     
     }
 
     /**
@@ -85,6 +155,7 @@ public class UploadInformationFilesServlet extends HttpServlet {
      */
     @Override
     public String getServletInfo() {
+        
         return "Short description";
     }// </editor-fold>
 
