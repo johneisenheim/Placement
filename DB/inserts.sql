80fd032017de056fe16d67b6c759b7a33188db49
carboma89 <carboma89@gmail.com>
2014-11-25 19:18:02 +0100

    Aggiornamento DB

--- DB/inserts.sql
+++ DB/inserts.sql
@@ -1,12 +1,11 @@
-INSERT INTO `mydb`.`Permissions` (`idPermissions`, `description`, `class`) VALUES ('1', 'L\'utente ha la tutti i permessi su tutti i sistemi', 'rwmd-rwmd-rwmd-rwmd');
+INSERT INTO `mydb`.`Permissions` (`idPermissions`, `description`, `class`) VALUES ('1', 'L\'utente ha la tutti i permessi su tutti i sistemi', '1-1-1-1-1');
 INSERT INTO `mydb`.`Account` (`idAccount`, `userName`, `password`, `typeOfAccount`, `FK_permission`) VALUES ('1', 'pippo', 'paperino', 'student', '1');
-INSERT INTO `mydb`.`StudentStatus` (`idStudentStatus`, `description`) VALUES ('1', 'Non richiesto');
-INSERT INTO `mydb`.`StudentStatus` (`idStudentStatus`, `description`) VALUES ('2', 'In attesa di conferma');
-INSERT INTO `mydb`.`StudentStatus` (`idStudentStatus`, `description`) VALUES ('3', 'Accettato');
-INSERT INTO `mydb`.`Department` (`idDepartment`) VALUES ('1');
-INSERT INTO `mydb`.`FisicPerson` (`idFisicPerson`, `name`, `lastName`) VALUES ('1', 'Valentino', 'Piglianculo');
-
-INSERT INTO `mydb`.`Student` (`serialNumber`, `FK_Account`, `FK_FisicPerson`, `FK_Department`, `FK_StudentStatus`) VALUES ('1', '1', '1', '1', '1');
+UPDATE `mydb`.`Department` SET `description`='DISTRA' WHERE `idDepartment`='1';
+INSERT INTO `mydb`.`FisicPerson` (`idFisicPerson`, `name`, `lastName`, `phoneNum`, `city`, `address`, `CAP`, `sex`, `citizenship`, `CF`) VALUES ('1', 'Valentio(PJ)', 'Vivone', '33135352', 'Battipaglia', 'Viale dei PJ', '89040', 'M', 'Italiano', 'SFAK231mKN');
+INSERT INTO `mydb`.`Student` (`serialNumber`, `yearEnrollment`, `cycle`, `universityEmail`, `FK_Account`, `FK_FisicPerson`, `FK_Department`) VALUES ('1', '2008', 'Magistrale', 'v.vivone.pj@studenti.unisa.it', '1', '1', '1');
+INSERT INTO `mydb`.`StudentInformation` (`idStudentInformation`, `CurriculumVitaePATH`, `AccademicTranscriptPATH`) VALUES ('1', 'c:\\usr\\utenti\\platform\\1100052200\\cv.pdf', 'c:\\usr\\utenti\\platform\\1100052200\\AT.pdf');
+INSERT INTO `mydb`.`StudentAttendence` (`SerialNum`, `Date`, `FK_Student`) VALUES ('1', '2015/12/01', '1');
+UPDATE `mydb`.`Student` SET `FK_StudentStatus`='2', `FK_idStudentInformation`='1' WHERE `serialNumber`='1';
 
 
 
